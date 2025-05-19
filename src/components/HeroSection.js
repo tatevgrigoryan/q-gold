6bcb5965531b58691ef92304a97c5099c58dac3f
tatev <tatevg1991@gmail.com>
2025-05-19 18:02:57 +0200

    issue with loading

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -22,25 +22,31 @@ function HeroSection() {
 
     return (
         <div className="hero-section">
-            <Carousel fade activeIndex={index} onSelect={handleSelect} controls indicators interval={4000}>
-                {slides.map((slide, idx) => {
-                    const { title, image } = slide;
-                    const imageUrl = `http://localhost:1337${image?.url}`;
+            {slides ? (
+                <Carousel fade activeIndex={index} onSelect={handleSelect} controls indicators interval={4000}>
+                    {slides.map((slide, idx) => {
+                        const { title, image } = slide;
+                        const imageUrl = `http://localhost:1337${image?.url}`;
 
-                    return (
-                        <Carousel.Item key={slide.id}>
-                            <div
-                                className="hero-slide"
-                                style={{ height: '70vh', background: `url(${imageUrl}) center/cover no-repeat` }}
-                            >
-                                {<Container className="d-flex flex-column justify-content-center align-items-center h-100 text-white text-center">
-                                    <h2 className={`animate__animated ${index === idx ? 'animate__slideInUp' : ''}`}>{title}</h2>
-                                </Container>}
-                            </div>
-                        </Carousel.Item>
-                    );
-                })}
-            </Carousel>
+                        return (
+                            <Carousel.Item key={slide.id}>
+                                <div
+                                    className="hero-slide"
+                                    style={{ height: '70vh', background: `url(${imageUrl}) center/cover no-repeat` }}
+                                >
+                                    {<Container className="d-flex flex-column justify-content-center align-items-center h-100 text-white text-center">
+                                        <h2 className={`animate__animated ${index === idx ? 'animate__slideInUp' : ''}`}>{title}</h2>
+                                    </Container>}
+                                </div>
+                            </Carousel.Item>
+                        );
+                    })}
+                </Carousel>
+            ) : (
+                <div className="slider-placeholder">Loading...</div>
+            )}
+
+
         </div>
     );
 }
