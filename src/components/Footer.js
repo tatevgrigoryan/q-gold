1147ce0b0a2fa8c094f637ce942683eb48176272
tatev <tatevg1991@gmail.com>
2025-06-03 18:05:14 +0200

    logo change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <Container className="py-5">
                 <Row className="justify-content-between align-content-start footer-content">
                     <Col md={2}>
-                        <img className="middle-img" src="/images/logo.png" alt="image"/>
+                        <img className="middle-img" src="/images/logo.jpeg" alt="image"/>
                     </Col>
                     <Col md={3}>
                         <div>
