f0d234f1624d26ae7d7cb76ca7541073cb097c72
tatev <tatevg1991@gmail.com>
2025-06-24 11:20:59 +0200

    new changes

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -2,15 +2,6 @@ import { useEffect } from "react";
 
 export default function TradingViewChart() {
     useEffect(() => {
-        const container = document.getElementById("tradingview-widget");
-        if (!container) return;
-
-        container.innerHTML = ""; // Clear old content
-
-        const widgetDiv = document.createElement("div");
-        widgetDiv.className = "tradingview-widget-container__widget";
-        container.appendChild(widgetDiv);
-
         const script = document.createElement("script");
         script.src = "https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js";
         script.async = true;
@@ -25,17 +16,10 @@ export default function TradingViewChart() {
             underLineColor: "#d2b521",
             isTransparent: false,
             autosize: true,
-            largeChartUrl: ""
+            largeChartUrl: "",
         });
-
-        container.appendChild(script);
+        document.getElementById("tradingview-widget").appendChild(script);
     }, []);
 
-    return (
-        <div
-            id="tradingview-widget"
-            className="tradingview-widget-container"
-            style={{ width: "100%", height: "300px" }}
-        />
-    );
+    return <div id="tradingview-widget" />;
 }
